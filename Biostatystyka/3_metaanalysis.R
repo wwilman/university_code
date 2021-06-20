a3342ef4bd23ce8dc993d53db8641badd70e0220
Wiktoria Wilman <62251588+wwilman@users.noreply.github.com>
2021-06-20 12:06:39 +0200

    Update 3_metaanalysis.R

--- Biostatystyka/3_metaanalysis.R
+++ Biostatystyka/3_metaanalysis.R
@@ -1,5 +1,5 @@
 library(meta)
-file=read.table("BSM.csv", sep=",",header=T)
+file=read.table("3_BSM.csv", sep=",",header=T)
 fnc=metabin(file$Chory,file$N.all_chory.,file$Zdrowy,file$N.all_zdrowy.,data=file,sm="OR",method="I",comb.fixed = F, subset = Population =='Caucasian')
 summary(fnc)
 fna=metabin(file$Chory,file$N.all_chory.,file$Zdrowy,file$N.all_zdrowy.,data=file,sm="OR",method="I",comb.fixed = F, subset = Population =='Asian')
